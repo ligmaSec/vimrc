6d416196e78ae6e1468ea53f07ad7ebe73e6ec1d
Ligma <ligma@fbi.systems>
2026-03-21 02:18:18 +0100

    fixed lsp

--- lua/theprimeagen/lazy/lsp.lua
+++ lua/theprimeagen/lazy/lsp.lua
@@ -29,7 +29,6 @@ return {
                 "lua_ls",
                 "rust_analyzer",
                 "gopls",
-                "volar",
                 "tsserver",
             },
             handlers = {
